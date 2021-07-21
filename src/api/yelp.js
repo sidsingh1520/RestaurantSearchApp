459c214e2d5d6ca0cdb145bebbbe5a2fed57e720
Siddhartha Singh <85829161+sidsingh1520@users.noreply.github.com>
2021-07-21 09:15:13 +0530

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:'https://api.yelp.com/v3/businesses',
+    baseURL:'Your API',
     headers:{
-        Authorization: 'Bearer yn79mE6xZcHzSe6I07h4nN_eDLysGstSKWZ6_0JDB-QLz8ODM6d_ohu_PBgoMibNMh8wHTh0GoL0OYLlKLsZURPg9pDMKdVq37e0Dm6HY_LEJhbTXG0l8VmQK0hKYHYx'
+        Authorization: 'Bearer "Token"'
     }
 });
